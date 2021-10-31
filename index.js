bc33d0d6dab3b04c0fba5ca8e0043b0d5f63b31d
mahmud <picowf@gmail.com>
2021-10-31 20:21:36 +0600

    done

--- index.js
+++ index.js
@@ -1,15 +1,90 @@
 const express = require("express");
+const { MongoClient } = require("mongodb");
 const app = express();
 const cors = require("cors");
+const ObjectId = require("mongodb").ObjectId;
+const { json } = require("express");
 require("dotenv").config();
-const port = 5000;
+const port = process.env || 5000;
 
 // middleware
 app.use(cors());
 app.use(express.json());
 
+// connection to database
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.tkswl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const database = client.db("bookServices");
+    const serviceCollection = database.collection("services");
+    const userCollection = database.collection("users");
+
+    // post api
+    app.post("/services", async (req, res) => {
+      const service = req.body;
+      const result = await serviceCollection.insertOne(service);
+      res.json(result);
+    });
+
+    //post order api
+    app.post("/users", async (req, res) => {
+      const order = req.body;
+      const result = await userCollection.insertOne(order);
+      res.json(result);
+    });
+
+    //find data
+    app.get("/services", async (req, res) => {
+      const cursor = serviceCollection.find({});
+      const services = await cursor.toArray();
+      res.send(services);
+    });
+
+    // fond all orders
+    app.get("/allusers", async (req, res) => {
+      const cursor = userCollection.find({});
+      const alluser = await cursor.toArray();
+      res.send(alluser);
+    });
+
+    // delete user
+    app.delete("/allusers/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await userCollection.deleteOne(query);
+      res.json(result);
+    });
+
+    // delete order
+    app.delete("/services/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await serviceCollection.deleteOne(query);
+      res.json(result);
+    });
+
+    // get single service
+    app.get("/services/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+
+      const service = await serviceCollection.findOne(query);
+      res.json(service);
+    });
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("Running Book Services Services");
 });
 
 app.listen(port, () => {
